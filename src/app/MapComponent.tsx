b14e8eeebb86af4176e892a8b9e97f3a94fe6a72
Jason Wu <wu80jason8@gmail.com>
2023-11-11 14:38:15 -0800

    Add map component

---
+++ src/app/MapComponent.tsx
@@ -0,0 +1,57 @@
+'use client'
+import React, { useEffect, useRef } from 'react';
+import mapboxgl from 'mapbox-gl';
+
+mapboxgl.accessToken = 'pk.eyJ1Ijoicmstc2N1IiwiYSI6ImNsb3VqenEycTBjZnYyamxzZW03YXNwN3kifQ.8F1qz8MDLwT6Fnt3CmLQzQ';
+
+const MapComponent = () => {
+  const mapContainerRef = useRef(null);
+
+  useEffect(() => {
+    const map = new mapboxgl.Map({
+        container: mapContainerRef.current!,
+        style: 'mapbox://styles/mapbox/streets-v11',
+        center: [0, 0],
+        zoom: 1.5,
+    });
+
+    map.addControl(new mapboxgl.NavigationControl(), 'bottom-right');
+
+    const geolocate = new mapboxgl.GeolocateControl({
+      positionOptions: {
+        enableHighAccuracy: true,
+      },
+      trackUserLocation: true,
+    });
+
+    map.addControl(geolocate);
+
+    map.on('load', () => {
+      geolocate.trigger();
+    });
+
+    interface GeolocationPosition {
+        coords: {
+          latitude: number;
+          longitude: number;
+        };
+      }
+
+    geolocate.on('geolocate', (e) => {
+        const position = e as GeolocationPosition;
+        map.flyTo({
+            center: [position.coords.longitude, position.coords.latitude],
+            essential: true,
+            zoom: 10,
+        });
+    });
+      
+      
+
+    return () => map.remove();
+  }, []);
+
+  return <div ref={mapContainerRef} style={{ width: '100%', height: '100vh' }} />;
+};
+
+export default MapComponent;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import MapComponent from './MapComponent'
 
 export default function Home() {
   return (
@@ -90,6 +91,7 @@ export default function Home() {
           </p>
         </a>
       </div>
+      <MapComponent />
     </main>
   )
 }
